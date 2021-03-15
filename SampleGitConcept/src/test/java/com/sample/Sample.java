c19286aba7b0b25dbbfb59bde4c466caa8b037ac
Gomathi <Gomathi@LAPTOP-EHI1EESF>
2021-03-15 13:07:29 +0530

    First commit

---
+++ SampleGitConcept/src/test/java/com/sample/Sample.java
@@ -0,0 +1,18 @@
+package com.sample;
+
+import org.testng.annotations.Test;
+
+public class Sample {
+@Test
+public void createContact(){
+	System.out.println("createContact");
+}
+
+public void modifyContact(){
+	System.out.println("modifyContact");
+}
+
+public void deleteContact(){
+	System.out.println("deleteContact");
+}
+}
